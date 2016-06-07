ca62f5ab264ea13b5786ae7c981b59b52e397f32
acraig94 <acraig94@gmail.com>
2016-06-07 17:17:15 +1200

    Added Login Functionality

--- news/news/urls.py
+++ news/news/urls.py
@@ -1,27 +1,10 @@
-"""news URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
-from django.conf.urls import include
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'', include('np.urls')),
-    #url(r'^articles_nz', include('np.urls')),
-    #url(r'^articles_international', include('np.urls')),
-    #url(r'^articles_tech', include('np.urls')),
-    #url(r'^articles_sport', include('np.urls')),
 ]
--- news/np/views.py
+++ news/np/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Article
 from .forms import ArticleForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
-    articles = Article.objects.all().order_by('-createdDate')
+    articles = Article.objects.all().order_by('-createdDate')#[5] #annotate
     return render(request, 'np/article_list.html', {'articles': articles})
 
 def nz(request):
@@ -26,6 +27,8 @@ def details(request, pk):
     article = get_object_or_404(Article, pk=pk)
     return render(request, 'np/article.html', {'article': article})
 
+@login_required
 def article_new(request):
     form = ArticleForm()
     return render(request, 'np/article_edit.html', {'form': form})
+
